f97fd15708d564cf7b8ee3e4e93ae3fede35b0b6
Julio <jchuraaca@unsa.edu.pe>
2023-12-30 17:18:39 -0500

    Probando la configuracion del servidor usando un script basico

---
+++ lab08/calcular.pl
@@ -0,0 +1,6 @@
+#!"C:\xampp\perl\bin\perl.exe"
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+print "<html><head><title>Hola</title></head><body><h1>Hola desde el script CGI</h1></body></html>";
