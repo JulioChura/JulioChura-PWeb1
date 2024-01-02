d4dd58c03cc35dbca1a0e76d345db55ae45275a4
Julio <jchuraaca@unsa.edu.pe>
2024-01-02 15:52:26 -0500

    Se corrige la llaves, ahora si se almacenan todas las  coincidencias

--- lab09/cgi-bin/consulta.pl
+++ lab09/cgi-bin/consulta.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 use CGI;
+use utf8;
+
 
 my $q = CGI->new;
 print $q->header('text/html');
@@ -24,21 +26,21 @@ BLOCK
 
 my %coincidencias;
 
-#^(\d{3})? : para que calzen tres numeros
-#(.+) :Busca cualquier palabra o frase
 
 my $archivo = 'Programas_de_Universidades.csv';
 open my $IN, '<', $archivo or die "<h1>No se pudo abrir el archivo: $!</h1>\n";
 
+my $valor = 0;
 my $llave;
 my $cadena;
 my $line;
 while ($line = <$IN>) {
     if ( $line =~ /^(\d{3})\|(.+)/ ) {
-        $llave = $1;
         $cadena = $2;
-    } if ( $cadena =~ /$name\|(.*)$periodo(\|.*){5}\|$departamento(\|.*){5}\|$denominacion(.*)/) {
-        $coincidencias{$llave} = $cadena; 
+    } if ( $cadena =~ /$name?\|(.*)$periodo?(\|.*){5}\|$departamento?(\|.*){5}\|$denominacion?(.*)/) {
+        $llave = $valor;
+        $coincidencias{$llave} = $cadena;
+        $valor = $valor + 1; 
     } 
 
 }
