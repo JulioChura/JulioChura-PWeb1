ace2d9eb0aacf5bf2ffe796938d52acf39534be4
Julio <jchuraaca@unsa.edu.pe>
2024-01-01 22:59:37 -0500

    Se valida que los campos esten llenos

--- lab09/cgi-bin/consulta.pl
+++ lab09/cgi-bin/consulta.pl
@@ -5,3 +5,26 @@ use CGI;
 
 my $q = CGI->new;
 print $q->header('text/html');
+
+my $name = $q->param("nombre");
+my $periodo = $q->param("periodo");
+my $departamento = $q->param("departamento");
+my $denominacion = $q->param("denominacion");
+
+print<<BLOCK;
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+ <link rel="stylesheet" type="text/css" href="../css/styles.css">
+ <title>Resultados </title>
+ </head>
+<body>
+BLOCK
+
+if (!($name eq "") && !($periodo eq "") && !($departamento eq "") && !($denominacion eq "") ) {
+
+} else {
+    print "<h1>Llene todos los campos!</h1>"
+}
+
