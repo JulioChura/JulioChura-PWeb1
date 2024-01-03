01210563dbdca6a5db55c5d425fe51309c5317b2
Julio <jchuraaca@unsa.edu.pe>
2024-01-02 22:17:53 -0500

    Se copio una version antigua y se hicieron correciones

--- lab09/cgi-bin/consulta.pl
+++ lab09/cgi-bin/consulta.pl
@@ -2,8 +2,11 @@
 use strict;
 use warnings;
 use CGI;
+use utf8;
 
-#probando
+
+
+
 my $q = CGI->new;
 print $q->header('text/html');
 
@@ -25,25 +28,26 @@ BLOCK
 
 my %coincidencias;
 
-#^(\d{3})? : para que calzen tres numeros
-#(.+) :Busca cualquier palabra o frase
 
 my $archivo = 'Programas_de_Universidades.csv';
 open my $IN, '<', $archivo or die "<h1>No se pudo abrir el archivo: $!</h1>\n";
 
+my $valor = 0;
 my $llave;
 my $cadena;
 my $line;
 while ($line = <$IN>) {
     if ( $line =~ /^(\d{3})\|(.+)/ ) {
-        $llave = $1;
         $cadena = $2;
-    } if ( $cadena =~ /$name\|(.*)$periodo(\|.*){5}\|$departamento(\|.*){5}\|$denominacion(.*)/) {
-        $coincidencias{$llave} = $cadena; 
+    } if ( $cadena =~ /^$name?(\|.*){2}\|$periodo?(\|.*){5}\|$departamento?(\|.*){5}\|$denominacion?(.*)/) {
+        $llave = $valor;
+        $coincidencias{$llave} = $line;
+        $valor = $valor + 1; 
     } 
 
 }
 
+close $IN;
 foreach my $var ( %coincidencias) {
     print "<h1> $var </h1>\n"
 }
