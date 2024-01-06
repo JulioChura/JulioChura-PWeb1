dc2140770820a024dd483c7bd9b348af09d598d1
Julio <jchuraaca@unsa.edu.pe>
2024-01-06 11:44:30 -0500

    Se crea la cabecera del cgi

---
+++ lab10/Ejercicio1/consultaActor.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use DBI;
+
+#Se establece la conexion con la base de datos
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.8.102";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
