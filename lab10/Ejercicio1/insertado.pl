7d7a310ac8bdade72316bfee5a965b51a9706bac
Julio <jchuraaca@unsa.edu.pe>
2024-01-06 16:02:32 -0500

    Se cambia la ubicacion y el gestor de base de datos

--- lab10/Ejercicio1/insertado.pl
+++ lab10/Ejercicio1/insertado.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!"C:\xampp\perl\bin\perl.exe"
 use warnings;
 use strict;
 use DBI;
@@ -6,7 +6,7 @@ use DBI;
 #Se establece la conexion con la base de datos
 my $user = 'alumno';
 my $password = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.8.102";
+my $dsn = "DBI:mysql:database=pweb1;host=192.168.8.102";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
 #Se prepara la consulta SQL para insertar datos en la tabla actor 
