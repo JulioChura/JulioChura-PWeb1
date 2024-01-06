d2410bfda10f3e0a67041bdcbb5bc26f7cb186d4
Julio <jchuraaca@unsa.edu.pe>
2024-01-06 15:59:19 -0500

    Se hizo el cambio de gestor de base de datos y se opta por trabajar el xampp y ya no en ubuntu server

--- lab10/Ejercicio1/consultaActor.pl
+++ lab10/Ejercicio1/consultaActor.pl
@@ -1,16 +1,16 @@
-#!/usr/bin/perl
+#!"C:\xampp\perl\bin\perl.exe"
 use warnings;
 use strict;
 use DBI;
+use CGI;
 
 my $q = CGI->new;
 print $q->header('text/html');
 
-
 #Se establece la conexion con la base de datos
 my $user = 'alumno';
 my $password = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.8.102";
+my $dsn = "DBI:mysql:database=pweb1;host=192.168.8.102";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
 
