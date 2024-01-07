95b003493ac62f129cc1e94ff6d2a37d3ed1f531
Julio <jchuraaca@unsa.edu.pe>
2024-01-06 22:51:42 -0500

    Se termino la actividad 4

--- lab10/Ejercicio4/cgi-bin/buscarPelicula.pl
+++ lab10/Ejercicio4/cgi-bin/buscarPelicula.pl
@@ -13,27 +13,38 @@ my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 #Se hace la salida en formato html
 
 my $q = CGI->new;
+my $year = $q->param('year');
 print $q->header('text/html');
+
 
 print<<BLOCK;
 <!DOCTYPE html>
-<html>
- <head>
- <meta charset="UTF-8">
- <link rel="stylesheet" type="text/css" href="../css/styles.css">
- <title>Resultados </title>
- </head>
-<body class="fondo-resultados">
-<h1>Películas mejor rankeadas</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+<body class="fondoPrincipal">
+    
+    <form action="buscarPelicula.pl">
+        <h1 class="cabecera">
+            Buscador de pelicula
+        </h1>
+        <div class="formulario">
+            <input type="text" class="campoLlenar" readonly>
+            <a href="../index.html" class='enviar'>Limpiar</a>
+        </div>
+    </form>
 BLOCK
 
 #Se hace la consulta
-my $score = 7;
-my $votes = 5000;
-my $sth = $dbh->prepare("SELECT * FROM  Movie WHERE Score > ? and Votes > ?");
-$sth->execute($score, $votes );
+my $sth = $dbh->prepare("SELECT * FROM  Movie WHERE Year > ?");
+$sth->execute($year );
 
 print<<BLOCK;
+        <br><br>
         <table>
             <tr>
                 <th>MovieID</th>
