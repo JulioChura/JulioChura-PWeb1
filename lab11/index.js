2872029b7344d0238c86cecad545592924389a66
Julio <jchuraaca@unsa.edu.pe>
2024-02-04 12:11:46 -0500

    Calculadora basica

--- lab11/index.js
+++ lab11/index.js
@@ -1,11 +1,16 @@
-var resultado = "capucchino"
-function cafetera(ing1) {
-    if (ing1=="leche") {
-        console.log(resultado);
-        return resultado;
-    } else {
-        console.log("Incorrecto");
-    }
-}
+//Vamos a capturar en input
+const txtn1 = document.querySelector("#n1");
+const txtn2 = document.querySelector("#n2");
+const respuesta = document.querySelector("#resp");
+const btnCalcular = document.getElementById("calcular");
 
-cafetera("leche");
+
+// para controlar los eventos hacemos uso de addEventListener(evento, name_funcion)
+btnCalcular.addEventListener("click", calcular);
+function calcular() {
+    const op1 = parseFloat(txtn1.value);
+    const op2 = parseFloat(txtn2.value);
+    let resp = op1+op2;
+    respuesta.innerText = resp;
+}
+//Para controlar los clicks 
